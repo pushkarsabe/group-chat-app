73c3ce0e8b584ad6662e3078ff25b74caf31edd1
Pushkar Sabe <pushkarsabe@gmail.com>
2025-10-20 16:28:28 +0530

    config file

--- util/database.js
+++ util/database.js
@@ -7,8 +7,8 @@ if (process.env.RENDER) {
 
     sequelize = new Sequelize(
         process.env.DB_DATABASE,
-        process.env.DB_USER,
-        process.env.DB_PASSWORD,
+        process.env.DB_USER, 
+        process.env.DB_PASSWORD, 
         {
             host: process.env.DB_HOST,
             port: process.env.DB_PORT,
